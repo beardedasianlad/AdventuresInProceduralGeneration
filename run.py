f1b811c78d826c143d0ced3c0f950ad6d2ea67f1
beardedasianlad <jonathantanchiujoo@gmail.com>
2019-10-27 16:40:29 +0800

    Changed methods to manually random the choices

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-import tracery
 import random
-from tracery.modifiers import base_english
 
 CMythGenres = ['Ex Nihilo','World Diver','Emergence', 'Creation via Dismemberment','Split/Order']
 GodNames = ['Emdos', 'Iren', 'Qhaais', 'Sierin','Xydos','Ynas','Conmos','Easis','Xuhanh','Phoaldin','Zubris','Dhovdite','Bodos','Vosarin','Imarin','Istar','Aion','Meotl','Matis','Myas','Udos','Cinean','Qytis','Mehrer']
@@ -10,25 +8,48 @@ WorldNames = ['Pluccoros','Blammaqar','Uddunor','Eossirat','Gleoddenem','Aeklino
 VoidNames = ['Void','Gap','Hollow','Emptyness','Null']
 CastleSynonyms = ['Acropolis','Alcazar','Citadel','Fort','Fortress','Hold','Keep','Manor','Palace', 'Safehold','Seat', 'Stronghold', 'Tower',  'Villa', 'Château']
 
-rules = {
-    'mythSynop': 'This is the tale of how #God#, the #Domain#-#Rank#, created the world of #World#.',
-    'WorldName' : WorldNames,
-    'GodName': GodNames,
-    'GodDomain' : DivineDomains,
-    'GodRank': Titles
-    'origin': '#[God:#GodName#][Domain:#GodDomain#][Rank:#GodRank#][World:#WorldName#]mythSynop'
-}
 
+class God:
+    def __init__(self):
+        self.Name=random.choice(GodNames)
+        self.Domain=random.choice(DivineDomains)
+        self.Rank=random.choice(Titles)
+        return;
+    def getName(self):
+        return self.Name
+    def getDomain(self):
+        return self.Domain
+    def getRank(self):
+        return self.Rank
 
+class Myth:
+    def __init__(self,CreatorGod):
+        Creator=CreatorGod
+        self.WorldName = random.choice(WorldNames)
+        self.genre = random.choice(CMythGenres)
+        return;
+    def printSynopsis(self):
+       print("This is the tale of how " + Creator.getName() + ", the" + Creator.getDomain() +"-" + Creator.getRank()+", created the world of "+ self.WorldName)
+       return;
 
-def randomMythGenre():
-    mythGenre = random.choice(CMythGenres)
-    print("Creation Myth Type is : " + mythGenre)
-    if mythGenre == 'Ex Nihilo'
-       exNihiloMyth =  {'creationMyth' : 'In the beginning, there was the #Void#. From their #Domain#-#Castle# they watched it for an eternity. Then, they gestured and spoke with voice without voice: "Let it be." And thus the world of #World# came to be.'}
-       exNihiloOrigin = {'origin':'[Domain:#GodDomain#][World:#WorldName#]creationMyth'}
-       rules.update(exNihiloMyth)
-       rules.update(exNihiloOrigin)
+class Example():
+    def main(self):
+        #randomMythGenre()
+        creatorGod = God()
+        cMyth = Myth(creatorGod)
+        cMyth.printSynopsis()
+        return;
+
+if __name__ == '__main__':
+    Example().main()
+#def randomMythGenre():
+  #  mythGenre = random.choice(CMythGenres)
+#    print("Creation Myth Type is : " + mythGenre)
+#    if mythGenre == 'Ex Nihilo'
+ #      exNihiloMyth =  {'creationMyth' : 'In the beginning, there was the #Void#. From their #Domain#-#Castle# they watched it for an eternity. Then, they gestured and spoke with voice without voice: "Let it be." And thus the world of #World# came to be.'}
+ #      exNihiloOrigin = {'origin':'[Domain:#GodDomain#][World:#WorldName#]creationMyth'}
+  #     rules.update(exNihiloMyth)
+  #     rules.update(exNihiloOrigin)
        
        
        #    elif mythGenre == 'World Diver'
@@ -40,18 +61,4 @@ def randomMythGenre():
  #   elif mythGenre == 'Split/Order'
 
  #   return
-
-
-
-
-
-
-def main():
-    randomMythGenre()
-    grammar = tracery.Grammar(rules)
-    grammar.add_modifiers(base_english)
-    print(grammar.flatten("#mythSynop#"))  # prints, e.g., "Hello, world!"
-    print(grammar.flatten('#creationMyth#'))
-
-
-if __name__ == "__main__": main()
+ 
